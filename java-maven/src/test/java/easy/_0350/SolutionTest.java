4c76c7cf10e349d264417196c284cf4b39b94559
gaojian <notice@myadream.com>
2022-05-17 00:01:48 +0800

    modify

--- java-maven/src/test/java/easy/_0350/SolutionTest.java
+++ java-maven/src/test/java/easy/_0350/SolutionTest.java
@@ -1,23 +1,81 @@
 package easy._0350;
 
-import org.junit.jupiter.api.Test;
+import easy.DataSet;
+import easy.DataSetControl;
+import kotlin.jvm.functions.Function1;
+import org.jetbrains.annotations.NotNull;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
 
-class SolutionTest {
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+
+@Test(groups = {"tags.array", "tags.hashTable", "tags.twoPointers", "tags.binarySearch", "tags.sorting", "difficulty.easy" })
+class SolutionTest extends DataSetControl {
 
     Solution solution = new Solution();
 
-    @Test
-    void intersect() {
-        int[] nums1 = new int[]{1, 2, 2, 1};
-        int[] nums2 = new int[]{2, 2};
+    @org.junit.jupiter.api.Test
+    void main() {
+        this.run();
+    }
 
-        assertArrayEquals(new int[]{2, 2}, solution.intersect(nums1, nums2));
+    @NotNull
+    @Override
+    public ArrayList<DataSet> buildDataSet() {
+        ArrayList<DataSet> dataSets = new ArrayList<DataSet>();
+        Assist assist = new Assist(new int[]{});
+        dataSets.add(
+                new DataSet(
+                        new int[]{1, 2, 2, 1},
+                        new int[]{2, 2,},
+                        assist.copy(new int[]{2, 2})
+                )
+        );
 
-        nums1 = new int[]{4, 9, 5};
-        nums2 = new int[]{9, 4, 9, 8, 4};
+        dataSets.add(
+                new DataSet(
+                        new int[]{4, 9, 5},
+                        new int[]{9 ,4},
+                        assist.copy(new int[]{9, 4, 9, 8, 4})
+                )
+        );
 
-        assertArrayEquals(new int[]{4, 9}, solution.intersect(nums1, nums2));
+        return dataSets;
+    }
+
+    @NotNull
+    @Override
+    public ArrayList<Function1<DataSet, Object>> buildImpl() {
+        ArrayList<Function1<DataSet, Object>> impls = new ArrayList<>();
+
+        impls.add(
+                dataSet -> {
+                    assertArrayEquals(
+                            (int[]) dataSet.getTarget(),
+                            solution.intersect(
+                                    (int[]) dataSet.getSample(),
+                                    ((Assist)dataSet.getAssist()).getNumber()
+                            ));
+
+                    return true;
+                }
+        );
+
+        impls.add(
+                dataSet -> {
+                    assertArrayEquals(
+                            (int[]) dataSet.getTarget(),
+                            solution.intersect2(
+                                    (int[]) dataSet.getSample(),
+                                    ((Assist)dataSet.getAssist()).getNumber()
+                            ));
+
+                    return true;
+                }
+        );
+
+        return impls;
     }
 }
