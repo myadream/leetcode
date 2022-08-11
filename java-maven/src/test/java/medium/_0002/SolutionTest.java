b8c12daafa9938524691c86530e6bed763660d5e
myadream <notice@myadream.com>
2022-08-12 01:57:12 +0800

    add 0278

--- java-maven/src/test/java/medium/_0002/SolutionTest.java
+++ java-maven/src/test/java/medium/_0002/SolutionTest.java
@@ -3,6 +3,7 @@ package medium._0002;
 import easy.DataSet;
 import easy.DataSetControl;
 import kotlin.jvm.functions.Function1;
+import org.apache.commons.lang3.ArrayUtils;
 import org.jetbrains.annotations.NotNull;
 import org.testng.annotations.Test;
 
@@ -49,27 +50,53 @@ class SolutionTest extends DataSetControl {
                 item -> {
 
                     Assist assist = (Assist) item.getAssist();
+                    int[] A1 = assist.getL1();
+                    int[] A2 = assist.getL2();
+                    int[] A3 = (int[]) item.getTarget();
 
-                    Solution.ListNode L1 = new Solution.ListNode();
+                    ArrayUtils.reverse(A1);
+                    ArrayUtils.reverse(A2);
+                    ArrayUtils.reverse(A3);
+
+                    Solution.ListNode L1 = new Solution.ListNode(0, new Solution.ListNode());
                     for (int i:assist.getL1()) {
-                       L1.val = i;
-                       L1.next = new Solution.ListNode();
+                        L1.next.val = i;
+                        L1.next.next = new Solution.ListNode();
+                        L1.next = L1.next.next;
+                    }
+
+                    while (L1.val != 0) {
+                        L1 = L1.next;
+                        System.out.println(L1.val);
                     }
 
                     Solution.ListNode L2 = new Solution.ListNode();
                     for (int i:assist.getL2()) {
                        L2.val = i;
                        L2.next = new Solution.ListNode();
+                       L2 = L2.next;
                     }
 
-                    L1.next = null;
-                    L2.next = null;
+                    Solution.ListNode L3 = new Solution.ListNode();
+                    for (int i:(int[])item.getTarget()) {
+                        L3.val = i;
+                        L3.next = new Solution.ListNode();
+                        L3 = L3.next;
+                    }
+
+//                    L1.next = null;
+//                    L2.next = null;
+//                    L3.next = null;
 
                     Solution.ListNode listNode = (new Solution()).addTwoNumbers(L1, L2);
 
                     while (listNode.next != null) {
-                       System.out.println(listNode);
+
+                        System.out.println(listNode.val + "--"+L3.val);
+//                        assertEquals(listNode.val, L3.val);
+
                        listNode = listNode.next;
+                       L3 = L3.next;
                     }
 
                     return true;
