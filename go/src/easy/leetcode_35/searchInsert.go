a73dfd4797a6a5cb7006969bd38635cf79b167a6
QiYangXy <notice@myadream.com>
2023-06-30 01:04:17 +0800

    upadte

---
+++ go/src/easy/leetcode_35/searchInsert.go
@@ -0,0 +1,26 @@
+package leetcode704
+
+// 默认搜索
+func defaultSearch(nums []int, target int) int {
+	for i := 0; i < len(nums); i++ {
+		if nums[i] >= target {
+			return i
+		}
+	}
+
+	return len(nums)
+}
+
+func binarySearch(nums []int, target int) int {
+	l, r := 0, len(nums)
+	for l < r {
+		mid := l + (r - l) - 1
+		if nums[mid] >= target {
+			r = mid
+		} else {
+			l = mid + 1
+		}
+	}
+
+	return l
+}
---
+++ go/src/easy/leetcode_35/searchInsert_test.go
@@ -0,0 +1,76 @@
+package leetcode704
+
+import (
+	"fmt"
+	"leetcode/src/common"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func dataSet() []common.DataSet[common.DataSetParamAssist, common.DataSetTarget] {
+	var dataSets []common.DataSet[common.DataSetParamAssist, common.DataSetTarget]
+
+	dataSets = append(dataSets, common.DataSet[common.DataSetParamAssist, common.DataSetTarget]{
+		Param: common.DataSetParamAssist{
+			Value:  []int{1, 3, 5, 6},
+			Assist: 5,
+		},
+		Target: common.DataSetTarget{
+			Value: 2,
+		},
+	})
+
+	dataSets = append(dataSets, common.DataSet[common.DataSetParamAssist, common.DataSetTarget]{
+		Param: common.DataSetParamAssist{
+			Value:  []int{1, 3, 5, 6},
+			Assist: 2,
+		},
+		Target: common.DataSetTarget{
+			Value: 1,
+		},
+	})
+
+	dataSets = append(dataSets, common.DataSet[common.DataSetParamAssist, common.DataSetTarget]{
+		Param: common.DataSetParamAssist{
+			Value:  []int{1, 3, 5, 6},
+			Assist: 7,
+		},
+		Target: common.DataSetTarget{
+			Value: 4,
+		},
+	})
+
+	return dataSets
+}
+
+func handle(t *testing.T) []common.FunDataSet[common.DataSetParamAssist, common.DataSetTarget] {
+	var handle []common.FunDataSet[common.DataSetParamAssist, common.DataSetTarget]
+
+	//handle = append(handle, common.FunDataSet[common.DataSetParamAssist, common.DataSetTarget]{
+	//	HandlerDataSet: func(dataSet common.DataSetParamAssist, target common.DataSetTarget) bool {
+	//
+	//		res := defaultSearch(dataSet.Value.([]int), dataSet.Assist.(int))
+	//		assert.Equal(t, res, target.Value.(int), fmt.Sprintf("result: dataSet: %v, target: %v, res: %v", dataSet, target, res))
+	//
+	//		return true
+	//	},
+	//})
+
+	handle = append(handle, common.FunDataSet[common.DataSetParamAssist, common.DataSetTarget]{
+		HandlerDataSet: func(dataSet common.DataSetParamAssist, target common.DataSetTarget) bool {
+
+			res := binarySearch(dataSet.Value.([]int), dataSet.Assist.(int))
+			assert.Equal(t, res, target.Value.(int), fmt.Sprintf("result: dataSet: %v, target: %v, res: %v", dataSet, target, res))
+
+			return true
+		},
+	})
+
+	return handle
+}
+
+func TestBinarySearch(t *testing.T) {
+	test := common.DataSetControl[common.DataSetParamAssist, common.DataSetTarget]{}
+	test.Run(dataSet(), handle(t))
+}
