0b41c1b7eb983a24f54089c77e655d77848d8bfe
QiYangXy <notice@myadream.com>
2024-01-10 00:54:19 +0800

    add qeustion

---
+++ go/src/question/easy/leetcode_70/case_test.go
@@ -0,0 +1,100 @@
+package template
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"leetcode/src/common"
+	"testing"
+)
+
+func dataSet() []common.DataCarrier[common.DCDefault, common.TDefault] {
+	var dataSets []common.DataCarrier[common.DCDefault, common.TDefault]
+
+	dataSets = append(dataSets, common.DataCarrier[common.DCDefault, common.TDefault]{
+		SourceData: common.DCDefault{
+			Value:  2,
+			Assist: nil,
+		}, TargetData: common.TDefault{
+			Value: 2,
+		},
+	})
+
+	dataSets = append(dataSets, common.DataCarrier[common.DCDefault, common.TDefault]{
+		SourceData: common.DCDefault{
+			Value:  3,
+			Assist: nil,
+		}, TargetData: common.TDefault{
+			Value: 3,
+		},
+	})
+
+	return dataSets
+}
+
+func TestCaseOne(t *testing.T) {
+	for _, data := range dataSet() {
+		res := caseOne(data.SourceData.Value.(int))
+		assert.Equal(
+			t,
+			res,
+			data.TargetData.Value,
+			fmt.Sprintf(
+				"case one: dataSet: %v, target: %v, res: %v",
+				data,
+				data.TargetData.Value,
+				res,
+			),
+		)
+	}
+}
+
+func TestCaseTwo(t *testing.T) {
+	for _, data := range dataSet() {
+		res := caseTwo(data.SourceData.Value.(int))
+		assert.Equal(
+			t,
+			res,
+			data.TargetData.Value,
+			fmt.Sprintf(
+				"case one: dataSet: %v, target: %v, res: %v",
+				data,
+				data.TargetData.Value,
+				res,
+			),
+		)
+	}
+}
+
+func TestCaseThree(t *testing.T) {
+	for _, data := range dataSet() {
+		res := caseThree(data.SourceData.Value.(int))
+		assert.Equal(
+			t,
+			res,
+			data.TargetData.Value,
+			fmt.Sprintf(
+				"case one: dataSet: %v, target: %v, res: %v",
+				data,
+				data.TargetData.Value,
+				res,
+			),
+		)
+	}
+}
+
+func TestCaseFour(t *testing.T) {
+	for _, data := range dataSet() {
+		res := caseFour(data.SourceData.Value.(int))
+		assert.Equal(
+			t,
+			res,
+			data.TargetData.Value,
+			fmt.Sprintf(
+				"case one: dataSet: %v, target: %v, res: %v",
+				data,
+				data.TargetData.Value,
+				res,
+			),
+		)
+	}
+}
